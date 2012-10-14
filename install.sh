7c26d83939d860727f89e052cde8bca36d70ef0c
cpfaff <ctpfaff@arcor.de>
2012-10-14 22:04:34 +0200

    better install sh file

--- install.sh
+++ install.sh
@@ -3,9 +3,17 @@
 # ruby version
 ruby_version=1.9.3
 
-depends_on=(par ruby1.8-dev vim-nox git-core exuberant-ctags)
+depends_on_packages=(par ruby1.8-dev vim-nox git-core exuberant-ctags)
 
 file_this_script_logfile=/tmp/my-vim-setup.log
+
+folder_vim_vim=~/.vim
+file_vim_vimrc=~/.vimrc
+link_vim_vimrc_from=~/.vim/vimrc
+link_vim_vimrc_to=~/.vimrc
+
+my_vim_setup_cloneto=~/.vim
+vundle_clone_to=~/.vim/bundle/vundle
 
 # Messages
 message_intro=$(cat <<EOF
@@ -84,11 +92,9 @@ function execute_command()
 {
 	big_step_display "${1}"
 
-	${2}
+	${2} 2> /dev/null
 	check_for_process_error "Problems executing the command" ${2}
 }
-
-
 
 function backup_content()
 {
@@ -101,58 +107,55 @@ function backup_content()
 		check_for_process_error "Problems backing up the file" ${1}
 		rm -rf ${1}
 		check_for_process_error "Problems deleting the file" ${1}
-		
+
 	else
 		echo "Skipping backup"
 	fi
 }
 
 
-welcome_display "my-vim-setup script" "${message_intro}"
-sudo_install_packages ${depends_on[@]}
+function my_vim_setup()
+{
 
-folder_vim_vim=~/.vim
-file_vim_vimrc=~/.vimrc
-link_vim_vimrc_from=~/.vim/vimrc
-link_vim_vimrc_to=~/.vimrc
+welcome_display "my-vim-setup script" "${message_intro}"
+
+sudo_install_packages ${depends_on_packages[@]}
 
 backup_content ${folder_vim_vim}
 backup_content ${file_vim_vimrc}
 
-my_vim_setup_cloneto=~/.vim
-
-# Clone into my repository setup vundle
 execute_command "Clone into my-vim-setup repo" "git clone https://github.com/cpfaff/vim-my-setup.git ${my_vim_setup_cloneto}"
-
-vundle_clone_to=~/.vim/bundle/vundle
 execute_command "Clone into vundle repo" "git clone https://github.com/gmarik/vundle.git ${vundle_clone_to}"
 
-# Symbolic link the vimrc file
 execute_command "Link the .vimrc to user home" "ln -s ${link_vim_vimrc_from} ${link_vim_vimrc_to}"
 
 # Install the Bundles
-vim -u setup_vimrc -c :BundleInstall -c :q -c :q
+execute_command "Installing the bundles" "vim -u setup_vimrc -c :BundleInstall -c :q -c :q"
 
 # rvm ensure system ruby
 if which rvm 
 then
-	rvm use system
+	execute_command "Change to system ruby" "rvm use system"
 fi
 
 # Go to the comand-t build folder
-pushd ~/.vim/bundle/Command-T/ruby/command-t/
+pushd ~/.vim/bundle/Command-T/ruby/command-t/ > /dev/null
 	# Execute the extconf.rb file
-	ruby extconf.rb
-	make
+	execute_command "Execute the extconf" "ruby extconf.rb"
+	execute_command "Make command-t plugin" "make"
 popd
 
 # rvm reset ruby version
 if which rvm 
 then
-	rvm use ${ruby_version} 
+	execute_command "Change back to latest ruby" "rvm use ${ruby_version}"
 fi
 
 echo "Vim setup ready!"
+}
+
+# Function call
+my_vim_setup
 
 exit 0
 
