9e7eaa1698c60d5d9fee8369b036c53fa6e235ad
cpfaff <ctpfaff@arcor.de>
2012-10-15 16:00:41 +0200

    changed vim call

--- installer/install.sh
+++ installer/install.sh
@@ -128,7 +128,8 @@ function my_vim_setup()
 
 	execute_command "Link the .vimrc to user home" "ln -s ${file_home_vim_vimrc} ${file_home_vimrc}"
 
-	execute_command "Installing the bundles" "vim -u setup_vimrc -c :BundleInstall -c :q -c :q"
+	# execute_command "Installing the bundles" "vim -u setup_vimrc -c :BundleInstall -c :q -c :q"
+	execute_command "Installing the bundles" "vim +BundleInstall +qall"
 
 	if which rvm 
 	then
