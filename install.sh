0803cff74ba6da3d2447043af5353031eedbd1a4
cpfaff <ctpfaff@arcor.de>
2013-04-15 22:45:01 +0200

    Updates the installer

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 #{{{
 # Variable setting
 
-depends_on_packages=(make ack-grep par vim-nox git-core exuberant-ctags)
+depends_on_packages=(make ack-grep vim-nox git-core exuberant-ctags)
 apt_installer_preference=apt-get
 
 # Folder and files
@@ -21,7 +21,7 @@ file_this_script_logfile=/tmp/my-vim-setup.log
 
 message_intro=$(cat <<EOF
 Welcome to the my-vim-setup installation script. This script will install your
-complete vim setup using the vundle package.
+complete vim setup using the Neobundle package installer for Vim.
 EOF
 )
 
@@ -40,67 +40,66 @@ step_small="---------------------------"
 #{{{
 function write_log_entry()
 {
-	echo "${1}" >> ${file_this_script_logfile}	
+   echo "${1}" >> ${file_this_script_logfile}	
 }
 #}}}
 
 #{{{
 function check_for_process_error()
 {
-	if [ ${?} -eq "0" ]
-	then
-		write_log_entry "func: ${FUNCNAME[1]}: ${2} OK"  
-	else
-		write_log_entry "func: ${FUNCNAME[1]}: ${1} ${2}"  
-	fi	
+   if [ ${?} -eq "0" ]
+   then
+      write_log_entry "func: ${FUNCNAME[1]}: ${2} OK"  
+   else
+      write_log_entry "func: ${FUNCNAME[1]}: ${1} ${2}"  
+   fi	
 }
 #}}}
 
 #{{{
 function welcome_display()
 {
-	clear
-	echo ${frame_big}
-	echo -e "\t\t\t\t" "${1}"
-	echo ${frame_big}
-	echo ""
-	echo "${2}"
-	echo ""
-	echo ${frame_big} 
-	echo ""
+   clear
+   echo ${frame_big}
+   echo -e "\t\t\t\t" "${1}"
+   echo ${frame_big}
+   echo ""
+   echo "${2}"
+   echo ""
+   echo ${frame_big} 
+   echo ""
 }
 #}}}
 
 #{{{
 function big_step_display()
 {
-	echo ""
-	echo ${step_big}
-	echo "${1}"
-	echo ${step_big}
-	echo ""
+   echo ""
+   echo ${step_big}
+   echo "${1}"
+   echo ${step_big}
+   echo ""
 }
 #}}}
 
 #{{{
 function sudo_install_packages() 
 {
-	big_step_display "Install packages"
+   big_step_display "Install packages"
 
-	for package in "$@"  
-	do
-		if dpkg -s ${package} &> /dev/null
-		then 
-			echo "${package} already installed: skip"
-			echo ${step_small}
-		else 
-			echo "Installs" ${package}
-			sudo ${apt_installer_preference} -y install ${package} > /dev/null 
-			check_for_process_error "Problems installing the package:" ${package}
-			echo ${step_small}
-		fi
-
-	done
+   for package in "$@"  
+   do
+      if dpkg -s ${package} &> /dev/null
+      then 
+	 echo "${package} already installed: skip"
+	 echo ${step_small}
+      else 
+	 echo "Installs" ${package}
+	 sudo ${apt_installer_preference} -y install ${package} > /dev/null 
+	 check_for_process_error "Problems installing the package:" ${package}
+	 echo ${step_small}
+      fi
+   done
 }
 #}}}
 
@@ -117,44 +116,44 @@ function execute_command()
 #{{{
 function backup_content()
 {
-	big_step_display "Backup content"
+   big_step_display "Backup content"
 
-	echo "Backup" ${1}
-	if [ -e ${1} ]
-	then
-		cp -rf ${1}{,.bck}
-		check_for_process_error "Problems backing up the file" ${1}
-		rm -rf ${1}
-		check_for_process_error "Problems deleting the file" ${1}
+   echo "Backup" ${1}
+   if [ -e ${1} ]
+   then
+      cp -rf ${1}{,.bck}
+      check_for_process_error "Problems backing up the file" ${1}
+      rm -rf ${1}
+      check_for_process_error "Problems deleting the file" ${1}
 
-	else
-		echo "Skipping backup"
-	fi
+   else
+      echo "Skipping backup"
+   fi
 }
 #}}}
 
 #{{{
 function my_vim_setup()
 {
-	welcome_display "my-vim-setup script" "${message_intro}"
+   welcome_display "my-vim-setup script" "${message_intro}"
 
-	sudo_install_packages ${depends_on_packages[@]}
+   sudo_install_packages ${depends_on_packages[@]}
 
-	backup_content ${folder_home_vim}
-	backup_content ${file_home_dot_vimrc}
+   backup_content ${folder_home_vim}
+   backup_content ${file_home_dot_vimrc}
 
-	execute_command "Clone into my-vim-setup repo" "git clone https://github.com/cpfaff/vim-my-setup.git ${folder_home_vim}"
-  
+   execute_command "Clone into my-vim-setup repo" "git clone https://github.com/cpfaff/vim-my-setup.git ${folder_home_vim}"
+
    execute_command "Create bundle folder" "mkdir -p ${folder_home_vim_bundles}"
-  
+
    execute_command "Clone into neobundle repo" "git clone git://github.com/Shougo/neobundle.vim ${folder_home_vim_bundle_neobundle}"
 
-	execute_command "Link the .vimrc to user home" "ln -s ${file_home_vim_vimrc} ${file_home_dot_vimrc}"
-      
-	big_step_display "Install the bundles!"
+   execute_command "Link the .vimrc to user home" "ln -s ${file_home_vim_vimrc} ${file_home_dot_vimrc}"
+
+   big_step_display "Install the bundles!"
    vim -u ~/.vim/bundles.vim +NeoBundleInstall +qall
 
-	big_step_display "The Vim setup is ready!"
+   big_step_display "The Vim setup is ready!"
 }
 #}}}
 #}}}
